c19336db3af39332bda0139b18aca6f48edb15ba
Lyza-Jorella <[EMAIL]>
2022-12-17 03:55:28 +0800

    import networkx as nx 6

--- Using Queues in Practice.py
+++ Using Queues in Practice.py
@@ -462,3 +462,47 @@ def dijkstra_shortest_path(graph, source, destination, weight_factory):
                     previous[neighbor] = node
 
     return retrace(previous, source, destination)
+
+>>> import networkx as nx
+>>> from graph import City, load_graph, dijkstra_shortest_path
+
+>>> nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+>>> city1 = nodes["london"]
+>>> city2 = nodes["edinburgh"]
+
+>>> def distance(weights):
+...     return float(weights["distance"])
+...
+>>> for city in dijkstra_shortest_path(graph, city1, city2, distance):
+...     print(city.name)
+...
+City of London
+St Albans
+Coventry
+Birmingham
+Stoke-on-Trent
+Manchester
+Salford
+Preston
+Lancaster
+Carlisle
+Edinburgh
+
+>>> def weight(node1, node2, weights):
+...     return distance(weights)
+...
+>>> for city in nx.dijkstra_path(graph, city1, city2, weight):
+...     print(city.name)
+...
+City of London
+St Albans
+Coventry
+Birmingham
+Stoke-on-Trent
+Manchester
+Salford
+Preston
+Lancaster
+Carlisle
+Edinburgh
