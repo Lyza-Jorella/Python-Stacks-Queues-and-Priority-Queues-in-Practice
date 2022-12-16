0f134dc0f3807a4cfb9c49d33c4f0972882da784
Lyza-Jorella <[EMAIL]>
2022-12-17 02:31:06 +0800

    Using Queues in Practice (import networkx as nx 5)

--- Using Queues in Practice.py
+++ Using Queues in Practice.py
@@ -243,3 +243,17 @@ Aberdeen
 Inverness
 Lancaster
 ⋮
+
+>>> import networkx as nx
+>>> from graph import City, load_graph
+
+>>> nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+>>> city1 = nodes["aberdeen"]
+>>> city2 = nodes["perth"]
+
+>>> for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+...     print(f"{i}.", " → ".join(city.name for city in path))
+...
+1. Aberdeen → Dundee → Perth
+2. Aberdeen → Inverness → Perth
