d3c7441115e4e67b22a6147b8e6641311a254c10
Lyza-Jorella <[EMAIL]>
2022-12-17 01:09:26 +0800

    Using Queues in Practice (from typing import NamedTuple)

--- Using Queues in Practice.py
+++ Using Queues in Practice.py
@@ -11,3 +11,24 @@ MultiGraph named 'Cities in the United Kingdom' with 70 nodes and 137 edges
  'pos': '80,21!',
  'xlabel': 'City of London',
  'year': 0}
+
+# graph.py
+
+from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
