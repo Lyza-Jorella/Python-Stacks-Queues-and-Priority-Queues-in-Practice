f306fcbf58fb4734229bb8514d4f10d915c9705c
Lyza-Jorella <[EMAIL]>
2022-12-17 02:06:17 +0800

    Using Queues in Practice (from queues import Queue)

--- Using Queues in Practice.py
+++ Using Queues in Practice.py
@@ -182,3 +182,24 @@ Found: Lancaster 1937
 📍 Inverness
 📍 Sunderland
 Found: Sunderland 1992
+
+# graph.py
+
+from queues import Queue
+
+# ...
+
+def breadth_first_traverse(graph, source):
+    queue = Queue(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
+        if predicate(node):
+            return node
