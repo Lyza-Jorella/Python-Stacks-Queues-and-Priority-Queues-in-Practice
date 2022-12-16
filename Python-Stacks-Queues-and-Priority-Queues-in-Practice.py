534f6c2e72125a77e3b8dfc45c2b5fff9de63ef1
Lyza-Jorella <[EMAIL]>
2022-12-16 21:34:14 +0800

    Implementing Queues in Python (person)

--- Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
+++ Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
@@ -112,6 +112,16 @@ class Stack(Queue):
 >>> fruits
 ['banana', 'orange']
 
+>>> person1 = ("John", "Brown", 42)
+>>> person2 = ("John", "Doe", 42)
+>>> person3 = ("John", "Doe", 24)
+
+>>> person1 < person2
+True
+>>> person2 < person3
+False
+
+
 
 
 
