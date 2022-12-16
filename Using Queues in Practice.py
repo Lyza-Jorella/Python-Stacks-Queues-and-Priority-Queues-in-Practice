81316ca6d69b7faa8086f58d254a359e828e2395
Lyza-Jorella <[EMAIL]>
2022-12-16 23:13:58 +0800

    Using Queues in Practice ( import networkx as nx)

--- Using Queues in Practice.py
+++ Using Queues in Practice.py
@@ -1,3 +1,13 @@
 >>> import networkx as nx
 >>> print(nx.nx_agraph.read_dot("roadmap.dot"))
 MultiGraph named 'Cities in the United Kingdom' with 70 nodes and 137 edges
+
+>> > import networkx as nx
+>> > graph = nx.nx_agraph.read_dot("roadmap.dot")
+>> > graph.nodes["london"]
+{'country': 'England',
+ 'latitude': '51.507222',
+ 'longitude': '-0.1275',
+ 'pos': '80,21!',
+ 'xlabel': 'City of London',
+ 'year': 0}
