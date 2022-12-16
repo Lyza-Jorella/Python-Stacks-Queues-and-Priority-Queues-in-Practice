3a7da23daa61afaf51628032a5134dd272cc9685
Lyza-Jorella <[EMAIL]>
2022-12-16 20:48:48 +0800

    Implementing Queues in Python(queues.py 2)

--- Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
+++ Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
@@ -25,3 +25,24 @@ class Queue:
 '2nd'
 >>> fifo.dequeue()
 '3rd'
+
+# queues.py
+
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
