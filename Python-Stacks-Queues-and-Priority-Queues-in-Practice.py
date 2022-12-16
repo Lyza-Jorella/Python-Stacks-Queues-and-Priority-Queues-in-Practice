9ca7b30c6f9e1fd418e56ef7b21a0cffc4adcd41
Lyza-Jorella <[EMAIL]>
2022-12-16 20:09:11 +0800

    Implementing Queues in Python(fifo)

--- Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
+++ Python-Stacks-Queues-and-Priority-Queues-in-Practice.py
@@ -11,3 +11,17 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+>>> from queues import Queue
+
+>>> fifo = Queue()
+>>> fifo.enqueue("1st")
+>>> fifo.enqueue("2nd")
+>>> fifo.enqueue("3rd")
+
+>>> fifo.dequeue()
+'1st'
+>>> fifo.dequeue()
+'2nd'
+>>> fifo.dequeue()
+'3rd'
