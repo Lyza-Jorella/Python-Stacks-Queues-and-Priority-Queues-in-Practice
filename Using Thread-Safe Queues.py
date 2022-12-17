9ff35a2f62266884fb9464b61f8f9a6b44caa917
Lyza-Jorella <[EMAIL]>
2022-12-17 12:53:45 +0800

    thread_safe_queues.py (6)

--- Using Thread-Safe Queues.py
+++ Using Thread-Safe Queues.py
@@ -115,3 +115,17 @@ class Producer(Worker):
             self.simulate_idle()
 
 # ...
+
+# thread_safe_queues.py
+
+# ...
+
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
+
+# ...
