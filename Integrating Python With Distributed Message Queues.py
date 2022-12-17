123666df565e42b7d51d0948951f60e742ec2f68
Lyza-Jorella <[EMAIL]>
2022-12-17 23:11:23 +0800

    consumer.py (2)

--- Integrating Python With Distributed Message Queues.py
+++ Integrating Python With Distributed Message Queues.py
@@ -91,3 +91,12 @@ while True:
         topic="datascience",
         value=message.encode("utf-8"),
     )
+
+# consumer.py
+
+from kafka3 import KafkaConsumer
+
+consumer = KafkaConsumer("datascience")
+for record in consumer:
+    message = record.value.decode("utf-8")
+    print(f"Got message: {message}")
