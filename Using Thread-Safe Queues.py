474d9dbe28f587ddeb5521fe299ca2fa29357392
Lyza-Jorella <[EMAIL]>
2022-12-17 12:39:43 +0800

    thread_safe_queues.py (4)

--- Using Thread-Safe Queues.py
+++ Using Thread-Safe Queues.py
@@ -65,3 +65,34 @@ class Worker(threading.Thread):
         self.product = None
         self.working = False
         self.progress = 0
+
+# thread_safe_queues.py
+
+from random import randint
+from time import sleep
+
+# ...
+
+class Worker(threading.Thread):
+    # ...
+
+    @property
+    def state(self):
+        if self.working:
+            return f"{self.product} ({self.progress}%)"
+        return ":zzz: Idle"
+
+    def simulate_idle(self):
+        self.product = None
+        self.working = False
+        self.progress = 0
+        sleep(randint(1, 3))
+
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
+
