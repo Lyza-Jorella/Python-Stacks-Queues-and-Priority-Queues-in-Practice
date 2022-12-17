3744d499ad4cf5b028f4e5764c6cec7e7afc3827
Lyza-Jorella <[EMAIL]>
2022-12-17 23:00:16 +0800

    producer.py (2)

--- Integrating Python With Distributed Message Queues.py
+++ Integrating Python With Distributed Message Queues.py
@@ -78,3 +78,16 @@ services:
       - ALLOW_PLAINTEXT_LISTENER=yes
     depends_on:
       - zookeeper
+
+
+# producer.py
+
+from kafka3 import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    message = input("Message: ")
+    producer.send(
+        topic="datascience",
+        value=message.encode("utf-8"),
+    )
